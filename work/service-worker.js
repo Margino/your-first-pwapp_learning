2b8214723a9b6ecc01b9e530fe2c4e6d3bd7144b
Alex Petrov <i@petrov.com.ru>
2018-09-16 20:40:45 +0300

    Use service workers to cache the data

--- work/service-worker.js
+++ work/service-worker.js
@@ -1,4 +1,5 @@
 const cacheName = 'weatherPWA-step-6-1';
+const dataCacheName = 'weatherData-v1';
 const filesToCache = [
     '/',
     '/index.html',
@@ -36,7 +37,7 @@ self.addEventListener('activate', (e) => {
     caches.keys()
         .then((keyList) => {
             return Promise.all(keyList.map((key) => {
-                if (key !== cacheName) {
+                if (key !== cacheName && kay !== dataCacheName) {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
@@ -48,12 +49,72 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
     console.log('[Service worker] Fetch', e.request.url);
+    const dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        /*
+         * When the request URL contains dataUrl, the app is asking for fresh
+         * weather data. In this case, the service worker always goes to the
+         * network and then caches the response. This is called the "Cache then
+         * network" strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+         */
+         e.respondWith(
+             caches.open(dataCacheName)
+                .then((cache) => {
+                    return fetch(e.request)
+                        .then((response) => {
+                            cache.put(e.request.url, response.clone());
+                            return response;
+                        });
+                })
+         );
+    } else {
+        /*
+         * The app is asking for app shell files. In this scenario the app uses the
+         * "Cache, falling back to the network" offline strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+         */
+         e.respondWith(
+             // caches.match() evaluates the web request that triggered the fetch event, and checks to see if it's available in the cache
+             caches.match(e.request)
+                 .then((response) => {
+                     // responds with the cached version, or uses fetch to get a copy from the network
+                     return response || fetch(e.request);
+                 })
+         );
+    }
+});
+
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
     e.respondWith(
-        // caches.match() evaluates the web request that triggered the fetch event, and checks to see if it's available in the cache
-        caches.match(e.request)
-            .then((response) => {
-                // responds with the cached version, or uses fetch to get a copy from the network
-                return response || fetch(e.request);
-            })
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
     );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
