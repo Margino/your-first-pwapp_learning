a0c061a7e4d305533dbb88d77c272d74b2cd565a
Alex Petrov <i@petrov.com.ru>
2018-09-16 19:24:41 +0300

    Cache the site assets

---
+++ work/service-worker.js
@@ -0,0 +1,47 @@
+const cacheName = 'weatherPWA-step-6-1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/styles/inline.css',
+    '/images/clear.png',
+    '/images/cloudy-scattered-showers.png',
+    '/images/cloudy.png',
+    '/images/fog.png',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/partly-cloudy.png',
+    '/images/rain.png',
+    '/images/scattered-showers.png',
+    '/images/sleet.png',
+    '/images/snow.png',
+    '/images/thunderstorm.png',
+    '/images/wind.png'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service worker] Install');
+    e.waitUntil(
+        caches.open(cacheName)
+            .then((cache) => {
+                console.log('[Service worker] Caching app shell');
+                return cache.addAll(filesToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys()
+        .then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+  );
+  return self.clients.claim(); //fixes a corner case in which the app wasn't returning the latest data
+});
